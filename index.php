bb13ac1fd8c840c823576f777723e1f97468b084
Renan de Souza <renan.souza@setinet.com.br>
2021-06-11 12:22:32 -0300

    Print more variables to find the user's IP

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 
-echo "REMOTE_ADDR: " . (isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : "");
-
+echo "REMOTE_ADDR: " . (isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : "-") . "\n";
+echo "HTTP_X_FORWARDED_FOR: " . (isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : "-") . "\n";
+echo "HTTP_CLIENT_IP: " . (isset($_SERVER["HTTP_CLIENT_IP"]) ? $_SERVER["HTTP_CLIENT_IP"] : "-") . "\n";
